01d56db8435d014de3b6110099fecaecd7b4cd73
NasirNobin <nasir.nobin@gmail.com>
2018-05-19 05:29:23 +0600

    domain support

--- Scraper.php
+++ Scraper.php
@@ -6,14 +6,56 @@ use Sunra\PhpSimple\HtmlDomParser;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-class Scraper 
+class Scraper
 {
-	public static function client(){
-		return new Client();
-    }
-    
-	public static function html($html){
+	public $response;
+	public $headers;
+	public $client;
+
+	const DOMAIN = false;
+
+	public function client(){
+
+		if ( $this->client ) {
+			return;
+		}
+
+		$this->headers = [
+			'User-Agent' => 'Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+		];
+
+		$this->client = new Client([
+			'base_uri' => static::DOMAIN,
+			'headers'  => $this->headers,
+		]);
+
+		return $this->client;
+	}
+
+	public function get($url){
+		$this->response = $this->client()->get($url);
+
+		return $this;
+	}
+
+	function getBody(){
+		return $this->response->getBody();
+	}
+
+	function isHtml($html){
+		// return instamce_o;
+	}
+
+	public function html($html = ''){
+		if ( ! $html) {
+			$html = $this->response->getBody();
+		}
+
 		return HtmlDomParser::str_get_html( $html );
+	}
+
+	function url($url){
+		return rtrim(static::DOMAIN , '/' ) . '/' . ltrim($url);
 	}
 }
 
